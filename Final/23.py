458a88b9bc2c12506f5c8ad3eb100ad1c58dbdcd
shogo.baseball0306@gmail.com <shogo.baseball0306@gmail.com>
2025-03-15 10:51:39 -0700

    prime

---
+++ Final/23.py
@@ -0,0 +1,20 @@
+L, R = map(int, input().split())
+
+isprime = [True] * (R - L + 1)
+if L == 1:
+    isprime[0] = False
+    
+LIMIT = int(R**0.5)
+for i in range(2, LIMIT + 1):
+    min_value = ((L + i - 1) // i) * i
+    for j in range(min_value, R + 1, i):
+            if j == i:
+                continue
+            isprime[j - L] = False
+
+# 個数を数えて出力
+answer = 0
+for i in range(R - L + 1):
+	if isprime[i] == True:
+		answer += 1
+print(answer)
